d394508a24838ee7c31f3559b697eafc39caabe8
Ubuntu <ubuntu@ip-172-31-5-121.ec2.internal>
2022-08-22 20:08:34 +0000

    add status badge

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="ismailukman/microproject:v2.1.0"
+
+# Step 2:  
+# Authenticate & tag
+docker login
+docker tag microproject ${dockerpath}
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push ${dockerpath}
